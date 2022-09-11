3c9539726148b73c5a6a23855d3555d3c6ebd057
user.name <malikitimi@gmail.com>
2022-09-11 13:41:43 +0100

    value and variable

--- first assignment and challenge/values and variables/value and variable/main.js
+++ first assignment and challenge/values and variables/value and variable/main.js
@@ -1,3 +1,6 @@
 /**
  * first assignment is to assign value to a variable
+ * 1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
  */
