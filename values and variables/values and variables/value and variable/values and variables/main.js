1f5914d17c0d482c157414192c86889bddba4d61
user.name <malikitimi@gmail.com>
2022-09-15 13:58:20 +0100

    value and variable

--- values and variables/values and variables/value and variable/values and variables/main.js
+++ values and variables/values and variables/value and variable/values and variables/main.js
@@ -1,5 +1,11 @@
 /**
  * first assignment is to assign value to a variable
+ * @param(string) country
+ * 
+ * @param(string) continent
+ * 
+ * @param(string) population
+ * 
  * 1. Declare variables called 'country', 'continent' and 'population' and
 assign their values according to your own country (population in millions)
 2. Log their values to the console
