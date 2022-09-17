36f2d383de5b2e4e2da1f55c0f0212cb529e5b86
user.name <malikitimi@gmail.com>
2022-09-17 14:27:21 +0100

    value and variable

--- Assignments/main.js
+++ Assignments/main.js
@@ -21,7 +21,7 @@ let population = 270 + "million"
 
 /**
  * second assignment is to assign value to a variable
- * @param(string) country
+ * @param(bolean) isIsland
  * 
  * 
  1. Declare a variable called 'isIsland' and set its value according to your
@@ -29,4 +29,14 @@ country. The variable should hold a Boolean value. Also declare a variable
 'language', but don't assign it any value yet
 2. Log the types of 'isIsland', 'population', 'country' and 'language'
 to the console
+
+LECTURE: let, const and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
  */
+
+let isIsland = true;
+
