5502b62e0e4bb195d93acdd1984fa510d8beebfb
user.name <malikitimi@gmail.com>
2022-10-09 15:38:25 +0100

    Challenges

--- Assignments/main.js
+++ Assignments/main.js
@@ -19,46 +19,7 @@ let continent = "Africa";
 let population = 270 + "million"
 
 /**
- * *Challenge 1
-
-Mark and John are trying to compare their BMI (Body Mass Index), which is
-calculated using the formula:
-BMI = mass / height ** 2 = mass / (height * height) (mass in kg
-and height in meter).
-Your tasks:
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both
-versions)
-3. Create a Boolean variable 'markHigherBMI' containing information about
-whether Mark has a higher BMI than John.
-Test data:
-§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
-m tall.
-§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
-m tall.
-GOOD LUCk
- */
-
-// Mark data
-let markHeight = 1.69;
-let markMass = 78;
-
-// John
-
-let johnHeight = 1.95;
-let johnMass = 92;
-
-//MarkBMI
-
-markBMI = markMass / (markHeight * markHeight);
-console.log(markBMI);
-
-//JohnBMI
-
-johnBMI = markMass / (johnHeight * johnHeight);
-console.log(johnBMI);
-
-
+ * 
 /**
  * second assignment is to assign value to a variable
  * @param(bolean) isIsland
@@ -79,4 +40,3 @@ change, and which might change?). Then, change these variables to const.
  */
 
 let isIsland = true;
-
